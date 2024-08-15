37b0c76d68422fb2f7e17c41a06725797b2c6bf8
Björn Tenje Persson <bjorn@tenje.se>
2024-08-15 16:54:38 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, { Fragment } from "react";
 import './App.css';
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <main>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+      </main>
+    </Router>
   );
 }
 
