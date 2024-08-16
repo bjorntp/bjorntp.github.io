56d152ae1ff3479fe3b4e302b6e234e8b4bbaa2b
Björn Tenje Persson <bjorn@tenje.se>
2024-08-16 13:23:27 +0200

    renames and header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Fragment } from "react";
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./components/Header"
 import './App.css';
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <main>
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-        </nav>
-      </main>
-    </Router>
+    <div className="bg-[url('./assets/ahus_cropped.jpg')] w-screen h-screen">
+      <Header />
+      <Router>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
 export default App;
