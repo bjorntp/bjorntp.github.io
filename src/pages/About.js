55bd084a257d0319b08ec1251bdbff5546a0f063
Björn Tenje Persson <bjorn@tenje.se>
2024-08-17 23:11:02 +0200

    Uppdaterad "om" sida

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,18 +7,68 @@ function About() {
       <div className='col-start-3 col-span-2 m-5 flex flex-col content-between'>
         <img src={profil} alt="Björn" className='rounded-full mb-5' />
         <div className='bg-gray-200/30 backdrop-blur-md rounded-md p-2'>
-          <h3 className=''>Hejsan!</h3>
-          <p className=' mt-2'>Mitt namn är Björn, jag kommer från Kristianstad men har sedan 2019 bosatt mig i Lund tillsammans med min sambo.</p>
+          <h3 className='text-xl mb-2'>Hejsan!</h3>
+          <p className=''>Mitt namn är Björn, välkommen till min sida!</p>
         </div>
         <div className='bg-gray-200/30 backdrop-blur-md rounded-md mt-5 p-2'>
-          <h3 className=''>KONTAKTINFORMATION</h3>
-          <h4>Telefonnummber</h4>
+          <h3 className='text-xl mb-2'>KONTAKTINFORMATION</h3>
+          <h4 className='font-bold'>Telefonnummer</h4>
+          <p className='indent-1'>0727199689</p>
+          <h4 className='font-bold'>E-post</h4>
+          <p className='indent-1'><a href="mailto:bjorn@tenje.se">bjorn@tenje.se</a></p>
+          <h4 className='font-bold'>GitHub</h4>
+          <p className='indent-1'><a href="https://github.com/bjorntp">bjorntp</a></p>
         </div>
       </div>
-      <div className='col-start-5 col-span-3 bg-gray-200/30 backdrop-blur-md rounded-md m-5 p-2 '>
-        <h3>Utbildning</h3>
+      <div className='col-start-5 col-span-3'>
+        <div className='bg-gray-200/30 backdrop-blur-md rounded-md m-5 p-2 mx-auto w-full'>
+          <h2 className='text-3xl'>Björn Tenje Persson</h2>
+        </div>
+        <div className='bg-gray-200/30 backdrop-blur-md rounded-md m-5 p-2 mx-auto w-full'>
+          <h2 className='text-xl'>UTBILDNING</h2>
+          <h3 className='font-bold'>Lunds Tekniska Högskola</h3>
+          <p>2019 - pågående</p>
+          <p>Civilingenjör i information- och kommunikationsteknik</p>
+          <ul className='list-disc ml-4'>
+            <li>Programmeringsteknik och flertrådad programmering</li>
+            <li>Avancerad interraktionsdesign</li>
+            <li>Dator- och webbsäkerhet</li>
+          </ul>
+        </div>
+        <div className='bg-gray-200/30 backdrop-blur-md rounded-md m-5 p-2 mx-auto w-full'>
+          <h2 className='text-xl'>ARBETE</h2>
+          <h3 className='font-bold'>Teknikfokus Arbetsmässa (volontär)</h3>
+          <ul className='indent-2'>
+            <li>2021 - 2022</li>
+            <li>IT-ansvaring</li>
+            <li>Omskrivning av webbsidan samt en del av projektgruppen som skapade årets <a className='underline underline-offset-2' href="https://github.com/teknikfokus/fair.teknikfokus.se">onlineplattform</a>.</li>
+          </ul>
+          <h3 className='font-bold'>Sekreterare i bostadsrättsförening</h3>
+          <ul className='indent-2'>
+            <li>2019 - pågående</li>
+          </ul>
+          <h3 className='font-bold'>Mio möbler</h3>
+          <ul className='indent-2'>
+            <li>2017 - pågående</li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className='bg-gray-200/30 backdrop-blur-md rounded-md m-5 p-2 mx-auto w-full'>
+          <h2 className='text-xl'>KUNSKAPER</h2>
+          <h3 className='font-bold'>Inom IT</h3>
+          <ul className='indent-2'>
+            <li>Objektorienterad programmering i olika </li>
+            <li>Web programmering med JS/TS med flera ramverk</li>
+            <li>Databasprogrammering med SQL</li>
+          </ul>
+          <h3 className='font-bold'>Språk</h3>
+          <ul className='indent-2'>
+            <li>Svenska - modersmål</li>
+            <li>Engelska - flytande</li>
+          </ul>
+        </div>
       </div>
-
     </div>
   );
 }
