a6d30f29cc4aae557f7612e366ff8acc1d9fc023
Björn Tenje Persson <bjorn@tenje.se>
2024-08-17 23:06:37 +0200

    Ändrade till svenska

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ function Header() {
   return (
     <div className="w-screen justify-center grid grid-cols-3 pt-4 pb-4">
       <nav className="flex flex-row justify-around col-start-2 bg-gray-200/30 backdrop-blur-md rounded-md pt-2 pb-2">
-        <a href="/">Home</a>
-        <a href="about">About</a>
+        <a href="/">Hem</a>
+        <a href="about">Om mig</a>
       </nav>
     </div>
   );
